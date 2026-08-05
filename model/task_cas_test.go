fc946ae7ed27626f5dc06c2a7f17209891c9f449
xjdaniel <xjdaniel@aliyun.com>
2026-08-06 00:51:33 +0800

    feat(data): port upstream flow quota module (/api/data/flow)

--- model/task_cas_test.go
+++ model/task_cas_test.go
@@ -39,6 +39,7 @@ func TestMain(m *testing.M) {
 		&Token{},
 		&Log{},
 		&Channel{},
+		&QuotaData{},
 		&TopUp{},
 		&SubscriptionPlan{},
 		&SubscriptionOrder{},
@@ -58,6 +59,7 @@ func truncateTables(t *testing.T) {
 		DB.Exec("DELETE FROM tokens")
 		DB.Exec("DELETE FROM logs")
 		DB.Exec("DELETE FROM channels")
+		DB.Exec("DELETE FROM quota_data")
 		DB.Exec("DELETE FROM top_ups")
 		DB.Exec("DELETE FROM subscription_orders")
 		DB.Exec("DELETE FROM subscription_plans")
