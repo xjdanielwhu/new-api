3be278e9443b3b9a78c17727d7b4be520a9d7c93
xjdaniel <xjdaniel@gmail.com>
2026-08-14 10:47:19 +0800

    style(model-pricing): 5:5 layout split and fix editor panel overlap
    - balance model list / pricing editor columns to 5:5
    - narrow preview column in editor panel to avoid overlap
    - bump version to v2.0.0-rc-up02.23

--- web/default/src/features/system-settings/models/model-ratio-visual-editor.tsx
+++ web/default/src/features/system-settings/models/model-ratio-visual-editor.tsx
@@ -677,7 +677,7 @@ const ModelRatioVisualEditorComponent = forwardRef<
 
   return (
     <div className='flex flex-col gap-4'>
-      <div className='grid h-[clamp(720px,calc(100vh-12rem),900px)] min-h-0 gap-4 md:grid-cols-[minmax(0,1fr)_minmax(420px,0.82fr)] xl:grid-cols-[minmax(0,1.1fr)_minmax(520px,0.9fr)]'>
+      <div className='grid h-[clamp(720px,calc(100vh-12rem),900px)] min-h-0 gap-4 md:grid-cols-[minmax(0,1fr)_minmax(0,1fr)] xl:grid-cols-[minmax(0,1fr)_minmax(0,1fr)]'>
         <div className='flex min-h-0 min-w-0 flex-col gap-3'>
           <DataTableToolbar
             table={table}
@@ -724,7 +724,7 @@ const ModelRatioVisualEditorComponent = forwardRef<
               table={table}
               containerClassName='min-h-0 flex-1 rounded-md'
               tableContainerClassName='h-full'
-              tableClassName='table-fixed'
+              tableClassName='min-w-[852px] table-fixed'
               tableHeaderClassName='[&_tr]:border-b-0'
               splitHeaderScrollClassName='h-full'
               bodyContainerClassName='[scrollbar-gutter:stable]'
@@ -738,10 +738,10 @@ const ModelRatioVisualEditorComponent = forwardRef<
               colgroup={
                 <colgroup>
                   <col className='w-9' />
-                  <col className='w-[34%]' />
-                  <col className='w-[16%]' />
-                  <col className='w-[34%]' />
-                  <col className='w-[72px]' />
+                  <col className='w-[300px]' />
+                  <col className='w-[120px]' />
+                  <col className='w-[300px]' />
+                  <col className='w-auto' />
                 </colgroup>
               }
               renderRow={(row, { getCellClassName }) => (
